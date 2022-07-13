d9e29a08bafee242949583bf92edcc84c393439d
Nguyen Tuan <giantuan153@gmail.com>
2022-07-13 11:16:12 +0700

    Initial commit

--- app/src/main/java/com/example/coffee/activity/LoginActivity.java
+++ app/src/main/java/com/example/coffee/activity/LoginActivity.java
@@ -1,16 +1,90 @@
 package com.example.coffee.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.coffee.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText email,password;
+    private AppCompatButton btLogin;
+    private TextView tvRegister;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.etEmailLogin);
+        password = findViewById(R.id.etPasswordLogin);
+        btLogin = findViewById(R.id.btLogin);
+        tvRegister = findViewById(R.id.tvRegister);
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCanceledOnTouchOutside(false);
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strEmailLogin = email.getText().toString().trim();
+                String strPasswordLogin = password.getText().toString().trim();
+                if (TextUtils.isEmpty(strEmailLogin)){
+                    Toast.makeText(LoginActivity.this,"Vui lòng nhập email",Toast.LENGTH_SHORT).show();
+                }
+                else if (TextUtils.isEmpty(strPasswordLogin)){
+                    Toast.makeText(LoginActivity.this,"Vui lòng nhập mật khẩu",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    progressDialog.setMessage("Vui lòng đợi!");
+                    progressDialog.show();
+                    firebaseAuth.signInWithEmailAndPassword(strEmailLogin,strPasswordLogin)
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    progressDialog.dismiss();
+                                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                    assert user != null;
+                                    if (user.isEmailVerified()){
+                                        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                                        finish();
+                                    }
+                                    else {
+                                        user.sendEmailVerification();
+                                        Toast.makeText(LoginActivity.this,"Vui lòng xác thực email của bạn",Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(LoginActivity.this,"Đăng nhập thất bại",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/coffee/activity/RegisterActivity.java
+++ app/src/main/java/com/example/coffee/activity/RegisterActivity.java
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Parcel;
 import android.text.TextUtils;
 import android.view.View;
@@ -17,19 +19,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.coffee.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.OAuthCredential;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText email,password,rePassword;
     private AppCompatButton btRegister;
     private TextView login;
     private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +55,8 @@ public class RegisterActivity extends AppCompatActivity {
         btRegister = findViewById(R.id.btRegister);
         firebaseAuth = FirebaseAuth.getInstance();
         login = findViewById(R.id.tvLogin);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCanceledOnTouchOutside(false);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,10 +75,73 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this,"Vui lòng nhập mật khẩu",Toast.LENGTH_SHORT).show();
                 else if (TextUtils.isEmpty(strRepass))
                     Toast.makeText(RegisterActivity.this,"Vui lòng nhập lại mật khẩu",Toast.LENGTH_SHORT).show();
+                else if ( !strPass.equals(strRepass)){
+                    Toast.makeText(RegisterActivity.this,"Mật khẩu không khớp ",Toast.LENGTH_SHORT).show();
+                }
                 else {
-
+                    progressDialog.setMessage("Vui lòng đợi");
+                    progressDialog.show();
+                    firebaseAuth.createUserWithEmailAndPassword(strEmail,strPass)
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    progressDialog.dismiss();
+                                    uploadUserProfile();
+                                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                    user.reload();
+                                          user.sendEmailVerification()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()){
+                                                        Toast.makeText(RegisterActivity.this,"Đăng ký thành công! Vui lòng xác thực email",Toast.LENGTH_SHORT).show();
+                                                    }
+                                                    else {
+                                                        Toast.makeText(RegisterActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }
+                                            });
+                                    new Handler().postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                                            finish();
+                                        }
+                                    },1200);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(RegisterActivity.this,"Đăng ký không thành công",Toast.LENGTH_SHORT).show();
+                                }
+                            });
                 }
             }
         });
+    }
+
+    private void uploadUserProfile() {
+        String uid = firebaseAuth.getUid();
+        HashMap<String,Object> hashMap = new HashMap<>();
+        hashMap.put("uid",uid);
+        hashMap.put("user_name","");
+        hashMap.put("imgUser","");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
+        reference.child(""+uid)
+                .setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegisterActivity.this,"Đăng ký không thành công!",Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
